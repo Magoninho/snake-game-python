71402ade4f065c6c676f8968ed609ad15e4fe6a0
magoninho <magoninhogamer@gmail.com>
2020-12-03 17:48:16 -0300

    big updates

--- README.md
+++ README.md
@@ -12,9 +12,11 @@ After 4 years, I finally did the Snake game. Since I started programming (almost
 - [ ]  Try to find a way to use less If statements
 - [ ]  Object Oriented Programming version
 - [ ] Remake everything from scratch if necessary  
+- [x] Add a settings.json to edit the settings of the game
 **Game stuff:**  
-- [ ] Collision with the borders of the window
-- [ ] Sound effects?  
+- [x] Collision with the borders of the window
+- [x] Scores
+- [x] Change the font
 **Optional:**  
 - [ ]  Terminal version using curses or another thing
 
--- version1.py
+++ version1.py
@@ -1,21 +1,30 @@
 import pygame
+import json
 from pygame.locals import *
 import random
 
+settings = None
+"""
+OPENING THE SETTINGS FILE
+"""
+with open('./settings.json') as f:
+    settings = json.load(f)
 
 pygame.init()
 
 pygame.mixer.init(44000, -16, 1, 1024)
-pygame.mixer.music.load('./song2.wav')
+pygame.mixer.music.load(settings['song_path'])
 pygame.mixer.music.play(-1)
 
 
-WIDTH, HEIGHT = 300, 300
+WIDTH = settings['screen_size']
+HEIGHT = int(WIDTH / 4 * 4)
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("snake game")
 
 WHITE = (255, 255, 255)
-head_color = (255, 255, 0)
+fruit_color = settings['fruit_color']
+
 
 UP = 0
 RIGHT = 1
@@ -23,10 +32,10 @@ DOWN = 2
 LEFT = 3
 
 # snake stuff
-scale = 10
+scale = settings['scale']
 snk_x = WIDTH / 4 // scale * scale
 snk_y = HEIGHT / 2 // scale * scale
-tamanho_inicial = 5
+tamanho_inicial = settings['tamanho_inicial']
 
 moving_to = RIGHT
 
@@ -60,9 +69,12 @@ posicao_f = frutinha()
 
 def main():
     global snake, moving_to
+
+    scr = 0
+
     clock = pygame.time.Clock()
     while True:
-        clock.tick(15)
+        clock.tick(settings['frame_rate'])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -71,6 +83,11 @@ def main():
                     pygame.quit()
 
         SCREEN.fill((0, 0, 0))
+
+        font = pygame.font.Font("./8-BIT.TTF", 24)
+        text = font.render(
+            str(scr), False, (255, 255, 255))
+        SCREEN.blit(text, (0, 0))
 
         for square in range(len(snake) - 1, 0, -1):  # got stuck here a little bit
             snake[square] = [snake[square-1][0], snake[square-1][1]]
@@ -96,27 +113,43 @@ def main():
         if moving_to == DOWN:
             snake[0] = [snake[0][0], snake[0][1] + scale]
 
-
         # making the head stay with the body
         for body in range(len(snake)):
 
             pygame.draw.rect(SCREEN, (WHITE),
                              (snake[body][0], snake[body][1], scale, scale))
 
-        pygame.draw.rect(SCREEN, (255, 255, 0),
+        pygame.draw.rect(SCREEN, fruit_color,
                          (posicao_f[0], posicao_f[1], scale, scale))
 
         if snake[0][0] == posicao_f[0] and snake[0][1] == posicao_f[1]:
+            scr += 1
             score()
 
+        """
+        MAKE THE SNAKE COMEBACK WHEN GOING THROUGH THE WALL
+        """
         for i in range(len(snake) - 1, 0, -1):
+
+            if snake[0][0] > WIDTH - scale:
+                snake[0][0] = 0
+
+            if snake[0][0] < 0:
+                snake[0][0] = WIDTH - scale
+
+            if snake[0][1] > HEIGHT - scale:
+                snake[0][1] = 0
+
+            if snake[0][1] < 0:
+                snake[0][1] = HEIGHT - scale
+
             # if the snake collides with itself
             if snake[0] == snake[i]:
                 pygame.font.init()
-                font = pygame.font.SysFont("Arial", 24)
+                font = pygame.font.Font("8-BIT.TTF", 17)
                 text = font.render(
                     "Press R to respawn", False, (0, 255, 255))
-                SCREEN.blit(text, (0, 0))
+                SCREEN.blit(text, (5, HEIGHT-25))
                 morreu = True
                 while morreu:
                     for event in pygame.event.get():
